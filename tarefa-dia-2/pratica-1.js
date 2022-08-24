c96f1aa9c25dba64355cf2d8432563a26ebe5c4d
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-23 22:23:00 -0300

    Adicionado as saidas console para exibir os valores das variavéis criadas

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,10 @@ let email = "wanderlei2583@gmail.com"
 let profissao = "developer"
 let idade = 39
 let cidadeDosSonhos = "Bariloche"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(cidadeDosSonhos)
+
