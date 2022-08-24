8dd5bbddd423a23706b9fafa0abee545fa90bbfa
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-24 18:40:35 -0300

    Programa para gerar o valor total do produto.

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,29 @@
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+let taxaBrasil = 1.12
+let taxaIntenacional = 1.2
+
+if (produtoA.internacional == true) {
+  console.log('O produto, ' + produtoA.nome + ' tem o valor total de: R$' + produtoA.valor * taxaIntenacional)
+}
+else {
+  console.log('O produto, ' + produtoA.nome + ' tem o valor total de: R$' + produtoA.valor * taxaBrasil)
+}
+
+
+if (produtoB.internacional == true) {
+  console.log('O produto, ' + produtoB.nome + ' tem o valor total de: R$' + produtoB.valor * taxaIntenacional)
+}
+else {
+  console.log('O produto, ' + produtoB.nome + ' tem o valor total de: R$' + produtoB.valor * taxaBrasil)
+}
+
+
+if (produtoC.internacional == true) { 
+  console.log('O produto, ' + produtoC.nome + ' tem o valor total de: R$' + produtoC.valor * taxaIntenacional)
+}
+else {
+  console.log('O produto, ' + produtoC.nome + ' tem o valor total de: R$' + produtoC.valor * taxaBrasil)
+}
