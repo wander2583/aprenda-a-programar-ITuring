1cbfe92ebb8e1ef8370d6e29d09118c0d47cdab0
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-23 22:35:06 -0300

    Conhecendo os possivéis saidas para o console log do JS

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,9 +4,12 @@ let profissao = "developer"
 let idade = 39
 let cidadeDosSonhos = "Bariloche"
 
-console.log(nome)
-console.log(email)
-console.log(profissao)
-console.log(idade)
-console.log(cidadeDosSonhos)
+//console.log(nome)
+//console.log(email)
+//console.log(profissao)
+//console.log(idade)
+//console.log(cidadeDosSonhos)
 
+//console.log(nome, email, profissao, idade, cidadeDosSonhos)
+
+console.log("Olá,", nome,"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ",cidadeDosSonhos,". Confirma pra gente alguns dados? A sua idade é ",idade,", você é ",profissao," e o seu email é ",email,"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
