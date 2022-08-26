1ed86f2727bc776edef86392bba219ec8d455b93
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-25 22:55:29 -0300

    Praticando os numeros primos

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,9 @@
+// verificar se um número é primo...
+
+num = 4
+
+if (num % 1 === 0) {
+  console.log(`Sim, o número ${num} é primo!`)
+} else if (num % 2 === 0) {
+  console.log(`Sim, o número ${num} é primo!`)
+} 
