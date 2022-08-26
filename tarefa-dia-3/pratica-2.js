6e56c01a6f4ca982fda6cd10dae9db5079444d8d
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-25 21:20:21 -0300

    Adicionado a tarefa 2 do dia 03

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+// tabuada.
+let fix = 3 
+let num = 1
+
+console.log("Tabuada de multiplicar do número "+ fix)
+
+while (num <= 10) {
+  console.log("--> " + fix +" * "+ num +" = "+ fix * num )
+  num++;
+}
