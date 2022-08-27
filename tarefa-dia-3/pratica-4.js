ebf3fc79d9fdc68473bbe3bc89ad6ec243df9644
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-27 09:28:31 -0300

    Subindo codigo da tarefa 3 pratica 4

--- tarefa-dia-3/pratica-4.js
+++ tarefa-dia-3/pratica-4.js
@@ -1,7 +1,17 @@
 // verificar se um número é primo...
 
-num = 1
+let numero = 2
+let ehPrimo = `Sim, o número ${numero} é primo!`
 
-while (num <= 10000) {
-  if ( )
+if (numero === 1) {
+  ehPrimo = `Não, o número ${numero} não é primo!`
+} else if (numero === 2) {
+  ehPrimo = `Sim, o número ${numero} é primo!`
+} else {
+   for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+       ehPrimo = `Não, o número ${numero} não é primo!`
+    }
+  }
 }
+console.log(ehPrimo)
