86ac2ceaf1f82764abd0143038a0f8c197fbdfdd
Wanderlei Pereira <wanderlei2583@gmail.com>
2022-08-27 08:43:11 -0300

    Subindo parte da tarefa 4 e 5

--- tarefa-dia-3/pratica-4.js
+++ tarefa-dia-3/pratica-4.js
@@ -1,9 +1,7 @@
 // verificar se um número é primo...
 
-num = 4
+num = 1
 
-if (num % 1 === 0) {
-  console.log(`Sim, o número ${num} é primo!`)
-} else if (num % 2 === 0) {
-  console.log(`Sim, o número ${num} é primo!`)
-} 
+while (num <= 10000) {
+  if ( )
+}
